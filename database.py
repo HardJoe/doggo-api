1e3ea20f8b1c73ffed98fef97b6a8c77e4b84e14
HardJoe <jonathanamadeus95@gmail.com>
2022-03-17 15:48:26 +0700

    Separate database into database.py

---
+++ database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+# Create a sqlite engine instance
+engine = create_engine("sqlite:///dogs.db")
+
+# Create a DeclarativeMeta instance
+Base = declarative_base()
+
+
+class Dog(Base):
+    # Define To Do class inheriting from Base
+    __tablename__ = 'dogs'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(30), nullable=False)
+    breed = Column(String(50))
+    image = Column(String(100))
