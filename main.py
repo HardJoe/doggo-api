12a93205abfef0453414ce1ab5e692210b11cbde
HardJoe <jonathanamadeus95@gmail.com>
2022-03-17 15:24:55 +0700

    Move dog.py to main.py

--- main.py
+++ main.py
@@ -1,25 +1,7 @@
-import aiofiles
-
 from pydantic import BaseModel
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse, JSONResponse
 from fastapi_crudrouter import MemoryCRUDRouter as CRUDRouter
-
-
-app = FastAPI()
-# dogs = {
-#     "id": 1,
-#     "name": "Lady",
-#     "age": 3,
-#     "breed": "American Cocker Spaniel"
-# }
-# , {
-#     "name": "Tramp",
-#     "age": 3,
-#     "gender": 1,
-#     "breed": "mixed",
-#     "weight": 8
-# }
 
 
 class Dog(BaseModel):
@@ -28,10 +10,9 @@ class Dog(BaseModel):
     breed: str
 
 
-# lady = Dog(id=1, name="Lady", age=3, breed="spaniel")
-# tramp = Dog(id=2, name="Tramp", age=5, breed="mixed")
-# dogs = [lady, tramp]
-dogs = []
+app = FastAPI()
+app.include_router(CRUDRouter(schema=Dog))
+image_id = 1
 
 
 @app.get("/")
@@ -39,31 +20,19 @@ async def root():
     return {"message": "Welcome to doggo-api!"}
 
 
-@app.get("/dogs")
-async def get_dogs():
-    return {data: dogs
+@app.get("/images/{id}")
+async def get_image(id: int):
+    return FileResponse(f"./static/images/{id}.png")
 
 
-@ app.get("/dogs/{id}")
-async def get_dog(id: int):
-    if any(dog for dog in dogs if dog.id == id):
-        raise HTTPException(status_code=404, detail="Dog not found")
-    return {"dog": dogs[id]}
-
-
-# @app.post("/dogs")
-# def calculate(m: str = Form(...), f: str = Form(...)):
-#     return {
-#         "male": m,
-#         "female": f,
-#         "lovePercentage": randint(0, 100)
-#     }
-
-
-# @app.post("/loveanalysis")
-# def analyze(person: Person):
-#     return {
-#         "name": person.name,
-#         "age": person.age,
-#         "loveChance": randint(0, 100)
-#     }
+@app.post("/images")
+async def upload_file(file: UploadFile = File(...)):
+    global image_id
+    file_path = f"./static/images/{image_id}.png"
+    with open(file_path, 'wb') as image:
+        content = await file.read()
+        image.write(content)
+        image.close()
+    image_id += 1
+    return JSONResponse(content={"filename": f"{image_id}.png"},
+                        status_code=201)
